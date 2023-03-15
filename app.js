6cdf710eedd29c188ee84d8a8b9e99550f6c2c0d
Bhavuk <bhavuksharma2202@gmail.com>
2023-03-15 23:12:17 +0530

    EJS File created and basic server setup done
    - Basic app is created
    - Saving notes in an array in app.js for now
    - Notes are created dynamically in list.ejs using forEach

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const parser = require('body-parser');
+const path = require('path');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+
 // Path to static files (javascript or css)
-app.use(express.static(__dirname + "/public"));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 // To parse url encoded response from post request
 app.use(parser.urlencoded({extended: true}));
 
+
+var options = { weekday: 'long', month: 'long', day: 'numeric' };
+var today  = new Date();
+
+let currentDay = today.toLocaleDateString("en-US", options); // Saturday, September 17
+
+let items = [];
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.render('list', {dayName: currentDay, items: items});
 });
 
 app.post("/", (req, res) => {
-    const input = req.body.input;
-    console.log(input)
+    item = req.body.item;
+    items.push(item);
+    res.redirect("/");
 });
 
 const PORT = process.env.PORT || 5400;
